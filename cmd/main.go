05a1227bd6931316bd157a77068be9d47a6664c2
PatricioHeduan <heduanpatricio@gmail.com>
2021-12-21 15:41:34 -0300

    main and server files completed

---
+++ cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"wiselink/internal/server"
+
+	_ "github.com/joho/godotenv/autoload"
+)
+
+func main() {
+	serv, err := server.New("8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//Ejecutamos de forma concurrente el servidor
+
+	serv.Start()
+
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,41 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+	"wiselink/internal/server/routes"
+
+	"github.com/go-chi/chi"
+	_ "github.com/joho/godotenv/autoload"
+)
+
+//Devolvemos un puntero con nuestro server
+type Server struct {
+	server *http.Server
+}
+
+//Inicializamos el servidor y montamos los endpoints
+func New(port string) (*Server, error) {
+	//Estructura que funciona de mux
+	r := chi.NewRouter()
+
+	//Se monta como raiz la direccion "api"
+	r.Mount("/api", routes.New())
+
+	serv := &http.Server{
+		Addr:         ":" + port,
+		Handler:      r,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	//Construimos un server inicializado con el que acabamos de crear
+	server := Server{server: serv}
+	return &server, nil
+}
+
+func (serv *Server) Start() {
+	//log.Printf("Servidor corriendo en " + os.Getenv("PORT"))
+	log.Fatal(serv.server.ListenAndServe())
+}
